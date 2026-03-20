a5b7e6aec6dfae33c17202dca5fdececf12ba9a5
ajay2dhy <ajayahirwar2006@gmail.com>
2026-03-20 17:26:53 +0530

    Auto update

--- auto_update.bat
+++ auto_update.bat
@@ -4,24 +4,26 @@ echo =====================================
 echo   Running Rainfall DSS Auto Update
 echo =====================================
 
-REM Go to your project folder
+REM Activate Anaconda
+CALL "%USERPROFILE%\anaconda3\Scripts\activate.bat"
+CALL conda activate gfs
+
 G:
 cd "G:\My Drive\RAINFALL_FORECAST_INDIA\rainfall_web"
 
-REM Run Python rainfall pipeline
 echo Running rainfall model...
-"C:\Users\PC\anaconda3\python.exe" imd_gfs_icon_bias_corrected_rainfall_pipeline_FINAL_SAFE.py
+python imd_gfs_icon_bias_corrected_rainfall_pipeline_FINAL_SAFE.py
 
-REM Add updated files
 echo Adding files...
 git add .
 
-REM Commit with timestamp
+echo Pulling latest changes...
+git pull origin main --rebase
+
 echo Committing...
 git commit -m "Auto update"
 
-REM Push to GitHub
-echo Pushing to GitHub...
+echo Pushing...
 git push origin main
 
 echo =====================================
